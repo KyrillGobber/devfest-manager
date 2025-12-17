e1ee64a94339ec07a2b52fcf5493f1b4b63ace90
Ky <kyrill@gobber.ch>
2025-12-17 17:22:17 +0100

    d2m1 done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { API_URL } from './core/tokens';
 import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
+import { provideClientHydration, withEventReplay, withIncrementalHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +23,6 @@ export const appConfig: ApplicationConfig = {
         const src = config.src.replace('/images/', '');
         return `https://static-assets.dev/cdn-cgi/image/width=${config.width},format=auto/https://storage.googleapis.com/images-cdn-e0395.firebasestorage.app/${src}`;
       },
-    },
+    }, provideClientHydration(withIncrementalHydration()),
   ],
 };
